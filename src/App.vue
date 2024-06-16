26a32fde3a85a9d1502145c2803c73f41d5d2cc3
fayevivi <80673512+fayevivi@users.noreply.github.com>
2024-06-16 16:38:12 +0800

    get local Date and format

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template>
-  <main>home</main>
+  <main>
+    <!-- Date get local date-->
+    <div class="text-center mb-6">
+      {{
+        new Date().toLocaleDateString("en-US", {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })
+      }}
+    </div>
+  </main>
 </template>
